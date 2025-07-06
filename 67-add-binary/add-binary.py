bc7df9258313bf326d7670c5320a0a731a50b727
Neeraj R Menon <neerajrmenon004@gmail.com>
2025-07-06 15:39:26 +0530

    Time: 10 ms (14.09%) | Memory: 12.4 MB (84.61%) - LeetSync

---
+++ 67-add-binary/add-binary.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        deci1=0
+        deci2=0
+        bin1=""
+        sum1=0
+        if a=='0' and b=='0':
+            return '0'
+        else:
+        
+            for i in range(len(a)):
+                e=len(a)-1-i
+                deci1=deci1+int(a[i])*2**e
+
+            for i in range(len(b)):
+                e=len(b)-1-i
+                deci2=deci2+int(b[i])*2**e
+
+            sum1=deci1+deci2
+            print(deci1,deci2,sum1)
+
+            while sum1>1:
+                bin1=bin1+str((sum1)%2)
+                sum1=(sum1)/2
+            bin1=bin1+'1'
+
+            return bin1[::-1]
+
+
+
+"""
+        i=len(a)-1
+        while i>=0:
+            e=
+            deci=a[i]*2**e
+"""
